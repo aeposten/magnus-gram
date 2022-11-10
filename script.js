bc77e11292d76036a6a5686c4404abf551fbf1db
Amy <86963506+aeposten@users.noreply.github.com>
2022-11-10 09:22:25 -0800

    Can target and increment likes

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const POSTS = [
   {
+    id: 1,
     name: "Magnus T. Boxer",
     username: "magdog2020",
     location: "Baylands Nature Preserve Palo Alto, CA",
@@ -7,8 +8,10 @@ const POSTS = [
     post: "img/magnus-walk.jpeg",
     comment: "Exploring with my family!",
     likes: 871,
+    liked: false,
   },
   {
+    id: 2,
     name: "Magnus T. Boxer",
     username: "magdog2020",
     location: "Home California, USA",
@@ -16,8 +19,10 @@ const POSTS = [
     post: "img/magnus-table.jpeg",
     comment: "Surveying my domain",
     likes: 1008,
+    liked: false,
   },
   {
+    id: 3,
     name: "Magnus T. Boxer",
     username: "magdog2020",
     location: "Home California, USA",
@@ -25,31 +30,63 @@ const POSTS = [
     post: "img/magnus-birthday.jpeg",
     comment: "Celebrating my best pal's birthday",
     likes: 20000,
+    liked: false,
   },
 ];
 
 const mainEl = document.getElementById("main");
 
-function renderPosts() {
+function renderPostHTML() {
   const postSection = POSTS.map(
     (post) =>
-      `  <section class="post">
+      `  <section class="post" >
       <div class="post-header">
       <img class="avatar" src=${post.avatar} /> ${post.name} ${post.location}
       </div>
       <div class="post-img" style="background-image: url(${post.post})"> 
       </div>
       <div class="post-icons">
-        <span class="material-symbols-outlined"> favorite </span>
+        <span class="material-symbols-outlined"  onClick='incrementLikes(${JSON.stringify(
+          post
+        )})'> favorite </span>
         <span class="material-symbols-outlined"> chat_bubble </span>
         <span class="material-symbols-outlined"> mail </span>
       </div>
-      <div class="post-likes">${post.likes}</div>
+      <div class="post-likes" id="${post.id}">${post.likes}</div>
       <div class="post-content">${post.comment}</div>
     </section>`
-  ).join("");
+  );
 
-  mainEl.innerHTML = postSection;
+  return postSection;
 }
 
+function renderPosts() {
+  mainEl.innerHTML = renderPostHTML().join("");
+}
+
+function incrementLikes(post) {
+  const likesEl = document.getElementById(post.id);
+  if (!post.liked) {
+    post.likes++;
+    post.liked = true;
+    likesEl.textContent = post.likes;
+    // likesEl.textContent = post.likes
+  } else {
+    post.likes--;
+    post.liked = false;
+    likesEl.textContent = post.likes;
+  }
+ 
+  console.log(post.liked);
+}
 renderPosts();
+
+// console.log(postSection)
+// function incrementLikes(post) {
+//   // if (!post.liked) {
+//   //   post.likes++;
+//   //   post.liked = false;
+//   // }
+//   console.log(post.name);
+// }
+// const likeEl = document.getElementById(`like-${index}`)
